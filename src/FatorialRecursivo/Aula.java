28529a8f7c84977e520446fb1f6711336295c09b
januario86 <januario86fp@gmail.com>
2020-06-30 14:32:57 -0300

    Paradigma funcional

--- src/FatorialRecursivo/Aula.java
+++ src/FatorialRecursivo/Aula.java
@@ -1,10 +1,17 @@
 package FatorialRecursivo;
 
+import java.util.function.UnaryOperator;
+
 public class Aula {
 
     public static void main(String[] args) {
-        int valor = 10;
-        int resultado = valor * 3;
-        System.out.println("O resultado é::"+ resultado);
+
+        UnaryOperator<Integer> calcularValorVezesTrinta = valor -> valor * 3;
+        int valor =10;
+        System.out.println("O resultado é :: "+ calcularValorVezesTrinta.apply(10));
+
+        
+
+
     }
 }
