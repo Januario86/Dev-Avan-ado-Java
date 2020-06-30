cef2636de59e73da669058f9a40ce8a45de53e7c
januario86 <januario86fp@gmail.com>
2020-06-30 11:03:40 -0300

    Commit inicial, estrutura

---
+++ src/FatorialRecursivo/Aula.java
@@ -0,0 +1,10 @@
+package FatorialRecursivo;
+
+public class Aula {
+
+    public static void main(String[] args) {
+        int valor = 10;
+        int resultado = valor * 3;
+        System.out.println("O resultado é::"+ resultado);
+    }
+}
